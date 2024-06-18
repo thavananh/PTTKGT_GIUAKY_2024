24ac7bdf75499d465fa8a44c5e82fcb5b7e1cebe
John Doe <johndoe@example.com>
2024-06-19 02:01:00 +0700

    123123

--- DeQuy/DynamicPrograming/RodCuttingProblem.cpp
+++ DeQuy/DynamicPrograming/RodCuttingProblem.cpp
@@ -4,7 +4,12 @@
 #include <map>
 using namespace std;
 
-long deQuy(vector<int>& L, vector<int>& P, int input) {
+long deQuy(vector<int>& L, vector<int>& P, int input, map<int, int>& ghiNho) {
+    if (ghiNho.count(input) != 0)
+    {
+        return ghiNho[input];
+    }
+    
     if (input == 0)
     {
         return 0;
@@ -17,7 +22,7 @@ long deQuy(vector<int>& L, vector<int>& P, int input) {
     for (size_t i = 0; i < L.size(); i++)
     {
         int newInput = input - L[i];
-        int phanHoi = deQuy(L, P, newInput);
+        int phanHoi = deQuy(L, P, newInput, ghiNho);
         int sum = 0;
         if (phanHoi != -1)
         {
@@ -28,6 +33,7 @@ long deQuy(vector<int>& L, vector<int>& P, int input) {
             max = sum;
         }
     }
+    ghiNho[input] = max;
     return max;
 }
 
@@ -35,5 +41,6 @@ int main() {
     vector<int> L = {1, 2, 3, 4, 5, 6, 7, 8};
     vector<int> P = {1, 3, 7, 8, 12, 15, 16, 17};
     int input = 8;
-    cout << deQuy(L, P, input);
+    map<int, int> ghiNho;
+    cout << deQuy(L, P, input, ghiNho);
 }
